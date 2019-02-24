e9c7190364f9f3ea4663f6c4d0a23949cb88bfed
Daniel <danielmccormick99@gmail.com>
2019-02-24 13:20:44 -0800

    updated accessors

--- bidirectional_map.cpp
+++ bidirectional_map.cpp
@@ -4,9 +4,7 @@ namespace playground {
 
 	bidirectional_map::bidirectional_map(std::pair<S, T>) {
 		forwards = std::make_unique<std::unordered_map<S, T>>;
-		if (sizeof(S) != sizeof(T)) {
-			backwards = std::make_unique<std::unordered_map<T, S>>;
-		}
+		backwards = std::make_unique<std::unordered_map<T, S>>;
 	}
 
 	bidirectional_map::bidirectional_map(bidirectional_map &&other) {
@@ -14,10 +12,18 @@ namespace playground {
 		std::swap(backwards, other.backwards);
 	}
 
+	// NEEDS FIXING
 	bidirectional_map& operator=(bidirectional_map&& other) {
 		bidirectional_map::return(other);
 		return other; 
 	}
+
+	auto bidirectional_map::sbegin() { return forwards.begin() };
+	auto bidirectional_map::send() { return forwards.end() };
+	auto bidirectional_map::tbegin() { return backwards.begin() };
+	auto bidirectional_map::tend() { return backwards.begin() };
+
+
 
 
 }
