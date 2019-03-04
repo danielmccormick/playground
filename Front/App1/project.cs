e830ab48b77521806373caf47c13e4d0e2b9c494
Daniel <danielmccormick99@gmail.com>
2019-03-03 21:03:20 -0800

    initial c# form

---
+++ Front/App1/project.cs
@@ -0,0 +1,18 @@
+// A Hello World! program in C#.
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+public class Form1 : Form
+{
+    public Form1() { }
+
+    [STAThread]
+    public static void Main()
+    {
+        Application.EnableVisualStyles();
+        Application.Run(new Form1());
+
+    }
+}
