ecf9215680c71ca45c4890e4dbf50b106c30fba6
Daniel <danielmccormick99@gmail.com>
2019-03-04 22:04:48 -0800

    refactor

--- Front/App1/project.cs
+++ Front/App1/project.cs
@@ -7,15 +7,26 @@ using System.Windows.Forms;
 public class Form1 : Form
 {
     public  Form1() {
-        //this = new Form1();
+        
         Application.EnableVisualStyles();
+
+        this.WindowState = FormWindowState.Maximized;
+        this.TopMost = true;
+        this.BackColor = Color.FromArgb(15, 15, 80);
+
         button1 = new Button();
         button1.Size = new Size(40, 40);
         button1.Location = new Point(30, 30);
         button1.Text = "Click me";
         this.Controls.Add(button1);
-        //button1.Click += new EventHandler(button1_Click);
+        button1.Click += new EventHandler(button1_Click);
+
     }
+    private void button1_Click(object sender, EventArgs e)
+    {
+        MessageBox.Show("Hello World");
+    }
+
 
     public static void setProps(ref Form f) {
         f.WindowState = FormWindowState.Maximized;
@@ -26,10 +37,8 @@ public class Form1 : Form
     [STAThread]
     public static void Main()
     {
-        Form myForm = new Form();
-        setProps(ref myForm);
-        Application.Run(myForm);
-        //FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable;
+
+        Application.Run(new Form1());
     }
 
     public Button button1;
