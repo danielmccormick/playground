d4aa7c2d2c5d2bfbf379b121d04ba3034ca6427c
Daniel <danielmccormick99@gmail.com>
2019-03-04 21:36:34 -0800

    updated back window

--- Front/App1/project.cs
+++ Front/App1/project.cs
@@ -6,13 +6,31 @@ using System.Windows.Forms;
 
 public class Form1 : Form
 {
-    public Form1() { }
+    public  Form1() {
+        //this = new Form1();
+        Application.EnableVisualStyles();
+        button1 = new Button();
+        button1.Size = new Size(40, 40);
+        button1.Location = new Point(30, 30);
+        button1.Text = "Click me";
+        this.Controls.Add(button1);
+        //button1.Click += new EventHandler(button1_Click);
+    }
+
+    public static void setProps(ref Form f) {
+        f.WindowState = FormWindowState.Maximized;
+        f.TopMost = true;
+        f.BackColor = Color.FromArgb(15, 15, 80);
+    }
 
     [STAThread]
     public static void Main()
     {
-        Application.EnableVisualStyles();
-        Application.Run(new Form1());
-
+        Form myForm = new Form();
+        setProps(ref myForm);
+        Application.Run(myForm);
+        //FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable;
     }
+
+    public Button button1;
 }
