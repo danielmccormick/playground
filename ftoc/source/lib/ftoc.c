3294c8e4e6d14421c023a29cd9a3a47dfd187b97
Daniel McCormick <danielmccormick99@gmail.com>
2019-07-06 20:39:22 -0400

    fixed slight bug

--- ftoc/source/lib/ftoc.c
+++ ftoc/source/lib/ftoc.c
@@ -1,6 +1,7 @@
 #include "ftoc.h"
 
 #include <math.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 enum order { ONE, THOU, MIL, BIL };
@@ -9,9 +10,9 @@ const int order_int_map[] = {1, 1000, 1000000, 1000000000};
 const char order_char_map[] = {'C', 'K', 'M', 'B'};
 
 enum order getOrder(const float f) {
-	if (f > 1.0e9f) return BIL;
-	else if (f > 1.0e6f) return MIL;
-	else if (f > 1.0e3f) return THOU;
+	if (f >= 1.0e9f) return BIL;
+	else if (f >= 1.0e6f) return MIL;
+	else if (f >= 1.0e3f) return THOU;
 	return ONE;
 }
 
@@ -31,35 +32,39 @@ int ftrunc(const float f, const enum order o, const int i) {
 	} else if (i == 2) {
 		f_i %= 100;
 		f_i /= 10;
-		f_i *= 10;
+		//f_i *= 10;
 	} else if (i == 3) {
 		f_i %= 1000;
 		f_i /= 100;
-		f_i *= 100;
+		//f_i *= 100;
 	}
 	return f_i;
 }
 
 int getDecimal(float f,const enum order o, const int i) {
-	while (f >= order_float_map[o]) f -= order_float_map[o];
+	while (f >= order_float_map[o]) {
+		f -= order_float_map[o];
+	}
+	if (f < 0.5f) return 0;
 	while (f <= 10000.0f) f *= 10.0f;
 	int f_i = (int)f;
 	switch(i) {
-		case 1: while(f_i >= 10) f_i /= 10;
+		case 0: while(f_i >= 10) f_i /= 10;
 			return f_i;
 			break;
-		case 2: while (f_i >= 100) f_i /= 10;
+		case 1: while (f_i >= 100) f_i /= 10;
 			return (f_i%10);
 			break;
-		case 3: while (f_i >= 1000) f_i /= 10;
+		case 2: while (f_i >= 1000) f_i /= 10;
 			return (f_i%10);
 			break;
-		case 4: while (f_i >= 10000) f_i /= 10;
+		case 3: while (f_i >= 10000) f_i /= 10;
 			return (f_i%10);
 			break;
-		case 5: return (f_i%10);
+		case 4: return (f_i%10);
 			break;
-		default: exit(-1);
+		default: printf("Error: unexpected input to getDecimal: %i\n",i);
+			exit(-1);
 			break;
 	}
 }
@@ -70,14 +75,26 @@ int getDecimal(float f,const enum order o, const int i) {
  * param c : pointer to array of six chars. 
  */
 void fto6c(float f, char c[]) {
+	if (f < 0.1f) {
+		for (int i = 0; i < 6; i++) c[6] = '0';
+		return;
+	}
 	enum order magnitude = getOrder(f);
+	if (magnitude == BIL) {
+		c[0] = 'A'; c[1] = 'E'; c[2] = 'R'; c[3] = 'R'; c[4] = 'O'; c[5] = 'R';
+		return;
+	}
+	printf("Order calculated: %i\n", (int)magnitude);
 	int offset = precursor(f,magnitude);
-	c[offset] = order_char_map[offset];	
+	printf("Offset calculated: %i\n", offset);
+	c[offset] = order_char_map[magnitude];	
 	for (int i = 1; i <= offset; i++) {
+		printf("%i before offset yields %i\n", i, (int)ftrunc(f,magnitude,i));
 		c[offset-i] = (char)ftrunc(f,magnitude,i)+'0';
 	}
-	for (int i = 0; i+offset < 6; i++) {
-		c[i+offset] = (char)getDecimal(f,magnitude,i)+'0';
+	for (int i = 0; i+offset < 5; i++) {
+		printf("%i after offset yields %i\n", i,getDecimal(f,magnitude,i));
+		c[i+offset+1] = (char)getDecimal(f,magnitude,i)+'0';
 	}
 	return;
 }
---
+++ ftoc/source/test.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "10"
+./ftoc 10
+echo "1000"
+./ftoc 1000
+echo "1001"
+./ftoc 1001
+echo "10000000"
+./ftoc 10000000
+
